e5d700edeaa77437ac505176700ff0a3156ab59c
italo ricardo geske <italoricardogeske@gmail.com>
2019-06-15 15:55:09 +0900

    Initial Commit

---
+++ RicaWeb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RicaWeb
+//
+//  Created by Italo Ricardo Geske on 2019/06/15.
+//  Copyright © 2019 Italo Ricardo Geske. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
