a6bc961d5b1373580edc374516e87b91bfa391c8
italo ricardo geske <italoricardogeske@gmail.com>
2019-06-25 23:49:02 +0900

    余計なコードを削除・BookmarkのLayout調整

--- RicaWeb/History/HistoryViewController.swift
+++ RicaWeb/History/HistoryViewController.swift
@@ -29,12 +29,12 @@ class HistoryViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.rows[section].count
+        return viewModel.histories[section].count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Reuse", for: indexPath) as UITableViewCell
-        cell.textLabel?.text = viewModel.rows[indexPath.section][indexPath.row]
+        cell.textLabel?.text = viewModel.histories[indexPath.section][indexPath.row].url
         return cell
     }
 
@@ -47,7 +47,7 @@ class HistoryViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let url = viewModel.rows[indexPath.section][indexPath.row]
+        let url = viewModel.histories[indexPath.section][indexPath.row].url
         dismiss(animated: true, completion: {
             self.delegate?.didSelectHistory(url: url)
         })
--- RicaWeb/History/HistoryViewModel.swift
+++ RicaWeb/History/HistoryViewModel.swift
@@ -10,8 +10,7 @@ import Foundation
 
 class HistoryViewModel {
     public var sections: [String] = []
-    public var rows: [[String]] = [[]]
-    private var histories: [[History]] = []
+    public var histories: [[History]] = []
 
     func viewDidLoad() {
         setTable()
@@ -26,27 +25,19 @@ class HistoryViewModel {
         let histories = getHistories()
         if !histories.isEmpty {
             var dates: [String] = []
-            var urls: [[String]] = [[]]
             for history in histories {
                 let date = Util().dateFormatter(date: history.date as Date)
                 if !dates.contains(date) {
                     dates.append(date)
-                    urls.append([String]())
-                    //削除用
                     self.histories.append([History]())
                 }
-                urls[dates.count - 1].append(history.url)
-                //削除用
                 self.histories[dates.count - 1].append(history)
             }
             self.sections = dates
-            self.rows = urls
         }
     }
 
     func deleteRow(indexPath: IndexPath) {
-        rows[indexPath.section].remove(at: indexPath.row)
-        //DBの削除
         let historyToDelete = histories[indexPath.section].remove(at: indexPath.row)
         historyToDelete.mr_deleteEntity()
         NSManagedObjectContext.mr_default().mr_saveToPersistentStoreAndWait()
